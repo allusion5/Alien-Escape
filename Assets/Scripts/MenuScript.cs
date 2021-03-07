8fb16b6120835f37b25f84aace88117b917edb95
JorCar26 <70410997+JorCar26@users.noreply.github.com>
2021-03-07 03:22:59 -0500

    EvenMoreMenusandMusic
    The polishing touches

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
+    public GameObject ManagerofGame;
+    void Start()
+    {
+        ManagerofGame = GameObject.Find("GameManager");
+    }
+
+    void Update()
+    {
+        if(ManagerofGame != null) 
+        {
+            Destroy(ManagerofGame);
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
